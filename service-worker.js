d47f13491a64bed4a3c85d7fc6f287f1dab1f8c1
flxn05 <felixnowak7@gmail.com>
2025-05-16 17:13:57 +0200

    pwa

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+// Register a cache name
+const CACHE_NAME = 'scaling-fishstick-cache-v1';
+
+// List of files to cache
+const FILES_TO_CACHE = [
+  '/',
+  'scaling-fishstick/index.html',
+  'scaling-fishstick//styles.css', // Add your CSS file path
+  '/scaling-fishstick/script.js',  // Add your JS file path
+  '/scaling-fishstick/analyse.png',
+  '/scaling-fishstick/pro.png',
+  '/scaling-fishstick/product.png',
+  '/scaling-fishstick/home/index.html',
+  '/scaling-fishstick/home/71122-537102350_medium.mp4',
+  '/scaling-fishstick/home/product.jpg',
+  '/scaling-fishstick/home/styles.css',
+  '/scaling-fishstick/home/video_sea',
+];
+
+// Install event - cache files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event - serve cached content
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
